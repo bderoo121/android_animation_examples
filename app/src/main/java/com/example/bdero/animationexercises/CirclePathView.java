46b409d3d4a0a5bd4f69c35732ef913cb36e5a6a
Brendan Deroo <bderoo121@hotmail.com>
2018-05-07 09:28:19 -0400

    Add static circle path to background.

--- app/src/main/java/com/example/bdero/animationexercises/CirclePathView.java
+++ app/src/main/java/com/example/bdero/animationexercises/CirclePathView.java
@@ -4,46 +4,55 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
+import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 class CirclePathView extends View {
 
-    float viewWidth;
-    float viewHeight;
-    float radius;
-    Paint paint;
-    Path path;
+    Paint red_paint_fill = new Paint(), blue_paint_fill, green_paint_stroke, gray_paint_stroke;
 
     public CirclePathView(Context context) {
         super(context);
-        paint = new Paint();
-        path = new Path();
-
-        viewWidth = getWidth();
-        viewHeight = getHeight();
-        radius = Math.min(viewWidth, viewHeight);
+        init();
     }
 
-    public CirclePathView(Context context, AttributeSet attrs) {
+    public CirclePathView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        paint = new Paint();
-        path = new Path();
-
-        viewWidth = getWidth();
-        viewHeight = getHeight();
-        radius = Math.min(viewWidth, viewHeight);
+        init();
     }
 
+    public CirclePathView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+
+        red_paint_fill = new Paint();
+        red_paint_fill.setColor(Color.RED);
+        red_paint_fill.setStyle(Paint.Style.FILL);
+
+        blue_paint_fill = new Paint();
+        blue_paint_fill.setColor(Color.BLUE);
+        blue_paint_fill.setStyle(Paint.Style.FILL);
+
+        green_paint_stroke = new Paint();
+        green_paint_stroke.setColor(Color.GREEN);
+        green_paint_stroke.setStyle(Paint.Style.STROKE);
+        green_paint_stroke.setStrokeWidth(10);
+
+        gray_paint_stroke = new Paint();
+        gray_paint_stroke.setColor(Color.rgb(220, 220, 220));
+        gray_paint_stroke.setStyle(Paint.Style.STROKE);
+        gray_paint_stroke.setStrokeWidth(40);
+    }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        paint.setColor(Color.GRAY);
-        paint.setStrokeWidth(3);
+        canvas.drawCircle(380, 360, 190, gray_paint_stroke);
 
-        canvas.drawCircle(viewWidth, viewHeight, radius, paint);
     }
 }
